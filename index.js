9d8c4944550b449d11cd73c6492d258c410436b9
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-05 01:13:08 +0530

    create get and post routes

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+const videoRoutes = require("./routes/videos.js");
+const videoUploadRoute = require("./routes/videoUpload.js");
 
-const { PORT, SERVER_URL } = process.env;
-// app.listen("PORT", () => {
-//   console.log("HI");
-// });
-console.log(PORT);
-app.use = cors();
+app.use("/videos", videoRoutes);
+// app.use("/video-upload-page", videoUploadRoute);
 
-const videoRoutes = require("./routes/videos");
-app.use = ("/videos", videoRoutes);
+// const PORT = process.env.PORT || 8082;
+const CLIENT_URL = process.env.CLIENT_URL;
 
-app.post("/video-upload-page", (req, res) => {});
+app.listen(8082, () => {
+  // console.log(`listening on PORT ${PORT}`);
+  console.log(`listening on PORT `);
+});
+
+// app.use("/videos", express.static("./public"));
--- routes/videos.js
+++ routes/videos.js
@@ -1,13 +1,40 @@
-const Router = require("router");
-const router = require("express").Router();
-// Router.use();
+const express = require("express");
+const Router = require("Router");
+const router = express.Router();
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
-// router
-//   .route("/")
-//   .get((res, req) => {})
-//   .post((res, req) => {});
+const VIDEO_PATH = "./data/videos.json";
 
-router.get("/", function (res, req) {});
-router.get("/:videoid", function (res, req) {});
+const fetchVideos = () => {
+  const videosData = fs.readFileSync(VIDEO_PATH);
+  const parsedVideos = JSON.parse(videosData);
+  return parsedVideos;
+};
 
+router.get("/", (req, res) => {
+  const videosData = fetchVideos();
+  res.status(200).json(videosData);
+});
+
+router.get("/:videoid", (req, res) => {
+  const videosData = fetchVideos();
+  videoId = req.params.videoid;
+  filteredVideo = videosData.filter((video) => video.id === videoId);
+  res.status(200).json(filteredVideo);
+});
+
+router.post("/", (req, res) => {
+  const videoObj = req.body;
+  const newVideo = {
+    id: uuidv4(),
+    title: videoObj.title,
+    description: videoObj.description,
+  };
+
+  const videosData = fetchVideos();
+  videosData.push(newVideo);
+  fs.writeFileSync(VIDEO_PATH, JSON.stringify(videosData));
+  res.status(201).json(newVideo);
+});
 module.exports = router;
