b3306f2ca9f16f465f6f6383cfa9177e3cbd406a
jaskaurbrar14 <jaskaurbrar14@gmail.com>
2024-02-05 10:09:50 +0530

    final edit

--- routes/videos.js
+++ routes/videos.js
@@ -39,7 +39,7 @@ router.post("/", (req, res) => {
     id: uuidv4(),
     title: videoObj.title,
     channel: "Scotty Cranmer",
-    image: "http://localhost:8082/videos/images/image9.jpeg",
+    image: videoObj.image,
     description: videoObj.description,
     views: "3,092,284",
     likes: "75,985",
